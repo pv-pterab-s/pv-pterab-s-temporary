3a8eec6d00907465f4e50f00faeee6cbef46c18b
pv-pterab-s <75991366+pv-pterab-s@users.noreply.github.com>
2023-05-03 14:15:53 +0000

    chkpt

--- example.cpp
+++ example.cpp
@@ -1,36 +1,14 @@
-#include <arrayfire.h>
-#include <math.h>
-#include <stdio.h>
-#include <cstdlib>
-
-using namespace af;
-
-// create a small wrapper to benchmark
-static array A;  // populated before each timing
-static void fn() {
-    array B = fft2(A);  // 2d fft
-    B.eval();           // ensure evaluated
-}
+#include <arrayfire.h>             // > > > > > > > > > > > CLICK PLAY UPPER RIGHT
+const int N = (1 << 10);
+af::array A; void fn() { auto B = af::fft2(A); af::eval(B); }
 
 int main(int argc, char** argv) {
-    try {
-        int device = argc > 1 ? atoi(argv[1]) : 0;
-        setDevice(device);
-        info();
-
-        printf("Benchmark N-by-N 2D fft\n");
-        for (int M = 7; M <= 12; M++) {
-            int N = (1 << M);
-
-            printf("%4d x %4d: ", N, N);
-            A             = randu(N, N);
-            double time   = timeit(fn);  // time in seconds
-            double gflops = 10.0 * N * N * M / (time * 1e9);
-
-            printf(" %4.0f Gflops\n", gflops);
-            fflush(stdout);
-        }
-    } catch (af::exception& e) { fprintf(stderr, "%s\n", e.what()); }
-
-    return 0;
+    af::info();                                          // Show compute device info
+    A = af::randu(N, N);                                 // Generates uniform random matrix
+    af::array B = af::fft2(A);                           // Compute 2D FFT
+    af::array C = B(af::seq(0, 5), af::seq(0, 5));       // Grab 5x5 top left corner
+    af::array D = af::pinverse(C);                       // Pseudoinverse is easy
+    af_print(af::real(D));                               // Nicely see the results
+    printf("2D FFT %dx%d: %4.0f Gflops\n", N, N,         // Do a benchmark
+             10.0 * N * N * 10 / (af::timeit(fn) * 1e9));
 }
