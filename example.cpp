4907435f8b4f04480f2dbce19f8be241b6a7bba9
pv-pterab-s <75991366+pv-pterab-s@users.noreply.github.com>
2023-05-08 08:27:00 -0400

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -9,6 +9,6 @@ int main(int argc, char** argv) {
     af::array C = B(af::seq(0, 5), af::seq(0, 5));       // Grab 5x5 top left corner
     af::array D = af::pinverse(C);                       // Pseudoinverse is easy
     af_print(af::real(D));                               // Nicely see the results
-    printf("2D FFT %dx%d: %4.0f Gflops\n", N, N,         // Do a benchmark
-             10.0 * N * N * 10 / (af::timeit(fn) * 1e9));
+    printf("2D FFT %dx%d: %4.0f ms\n", N, N,             // Do a benchmark
+                            (af::timeit(fn) * 1e3));
 }
